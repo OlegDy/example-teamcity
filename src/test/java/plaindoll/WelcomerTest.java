f028f89ba02a61b423c312ef6380c18664059027
olegdy <olegdy@yandex.ru>
2023-03-12 17:02:28 +1000

    0.0.5 add_reply

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -30,4 +30,8 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+	@Test
+	public void welcomerGoodLuck(){
+		assertThat(welcomer.sayGLHunter(), containsString("hunter"));
+	}
 }
